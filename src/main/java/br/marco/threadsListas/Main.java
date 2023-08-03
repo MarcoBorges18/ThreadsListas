f89805665a793811eb8ce1dca501a12f850ca897
marco <antonioborges.marco@gmail.com>
2023-08-03 09:56:43 -0300

    third commit

--- src/main/java/br/marco/threadsListas/Main.java
+++ src/main/java/br/marco/threadsListas/Main.java
@@ -2,6 +2,10 @@ package br.marco.threadsListas;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("teste");
+        Lista lista = new Lista();
+
+        for (int i = 0; i < 10; i++) {
+            new Thread(new TarefaAdicionarElemento(lista));
+        }
     }
 }
---
+++ src/main/java/br/marco/threadsListas/TarefaAdicionarElemento.java
@@ -0,0 +1,12 @@
+package br.marco.threadsListas;
+
+public class TarefaAdicionarElemento implements Runnable {
+    public TarefaAdicionarElemento(Lista lista) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
